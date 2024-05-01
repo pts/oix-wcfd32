a32cd9d8b1cad943bbfed99f2baeada9b93dce0d
Peter Szabo <pts@fazekas.hu>
2024-05-01 08:37:02 +0200

    moved OIX demo programs written in NASM to asm_demo/

--- compile_wcfd32stub.sh
+++ compile_wcfd32stub.sh
@@ -10,18 +10,13 @@ cp -a run1/wcfd32stub run1/oixrun0.exe run1/oixrun0 run1/oixrun.oix run1/oixrun 
 
 nasm=run1/tools/nasm  # NASM 0.98.39 (2005-01-15) was the last version without amd64 (`bits 64') support. Integers are still 32-bit.
 
-# Example OIX exit(42) program.
-"$nasm" -O999999999 -w+orphan-labels -f bin -o answer42.oix answer42.nasm
-
-# Example OIX hello-world program.
-"$nasm" -O999999999 -w+orphan-labels -f bin -o hello.oix hello.nasm
-
-# Example OIX program which prints its command line and environment.
-"$nasm" -O999999999 -w+orphan-labels -f bin -o example.oix example.nasm
+"$nasm" -O999999999 -w+orphan-labels -f bin -o asm_demo/answer42.oix asm_demo/answer42.nasm
+"$nasm" -O999999999 -w+orphan-labels -f bin -o asm_demo/hello.oix    asm_demo/hello.nasm
+"$nasm" -O999999999 -w+orphan-labels -f bin -o asm_demo/example.oix  asm_demo/example.nasm
 
 #"$nasm" -O999999999 -w+orphan-labels -f bin -o wasm106.exe wasm106.nasm
 # Don't do it for oixrun.oix, because that would spoil the CF header in `oixrun'.
-for f in answer42.oix hello.oix example.oix wasmx100a.exe wasmx105.exe wasmx106.exe wasmx110b.exe wlibx105.exe wlibx106.exe wlibx110b.exe; do
+for f in asm_demo/*.oix wasmx100a.exe wasmx105.exe wasmx106.exe wasmx110b.exe wlibx105.exe wlibx106.exe wlibx110b.exe; do
   if test -f "$f"; then
     if test "${f%.oix}" = "$f"; then
       head="${f%x*.exe}"
