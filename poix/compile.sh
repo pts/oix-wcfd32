8425deaf9a3e99b52d319adeb63dc88a39eec789
Peter Szabo <pts@fazekas.hu>
2024-05-06 05:50:47 +0200

    refactor: added space after comment start

--- poix/compile.sh
+++ poix/compile.sh
@@ -10,7 +10,7 @@ NASM="${NASM:-nasm-0.98.39}"
 # USE_TRAMP_H. This uses the precached USE_TRAMP_H instead.
 "$NASM" -O999999999 -w+orphan-labels -f bin -o tramp.bin tramp.nasm
 perl -0777 -ne 's@(.)@sprintf("\\%03o", ord($1))@ges; print qq(const char tramp386[] = "$_";\n)' <tramp.bin >tramp.h
-ndisasm-0.98.39 -b 32 tramp.bin | perl -pe 's@^000000(\S+)\s+(\S+)\s+@my$a=$1;my$b=$2;$b=~s/(..)/\\x$1/g;my$c=" "x(22-length($b));"      /*\x400x$a*/  \"$b\"$c/*"@e&&s@$@ */@&&s@,@, @g' >tramp.inline.h
+ndisasm-0.98.39 -b 32 tramp.bin | perl -pe 's@^000000(\S+)\s+(\S+)\s+@my$a=$1;my$b=$2;$b=~s/(..)/\\x$1/g;my$c=" "x(22-length($b));"      /*\x400x$a*/  \"$b\"$c/* "@e&&s@$@ */@&&s@,@, @g' >tramp.inline.h
 # This works, no segfault: CC="$HOME"/prg/trusty.i386.dir/tmp/minilibc686/pathbin/minicc ./compile.sh --diet && ./oixrun; echo $?
 "$CC" -DUSE_TRAMP_H -s -O2 -W -Wall -m32 "$@" -o oixrun oixrun.c
 
